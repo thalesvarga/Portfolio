6af165ed6727639aa9ca4332210deb0457697d69
thalesvarga <thalesfotos@hotmail.com>
2025-03-13 03:03:51 -0300

    atualizacoes de responsavidade

--- landingpage.js
+++ landingpage.js
@@ -1,24 +1,34 @@
-/* ============ ENTRADA============ */
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   const entradaSite = document.getElementById("entrada-site");
-//   const conteudoSite = document.getElementById("conteudo-site");
+/* ============ HEADER MENUS SCROLL DOWN============ */
 
-//   // Inicia a animação após 0.5s
-//   setTimeout(() => {
-//     entradaSite.classList.add("fade-out");
-    
-//     // Remove completamente a entrada após 3s
-//     setTimeout(() => {
-//       entradaSite.style.display = "none";
-//       conteudoSite.classList.add("fade-in");
-//     }, 5000); // Tempo total = 0.5s + 3s
+const sectionOffsets = {
+  "skills": 50,
+  "trabalhos": 90,
+  "contato": 50,
+};
 
-//   }, 500);
-// });
+document.querySelectorAll(".header-menu li").forEach((item) => {
+  item.addEventListener("click", function () {
+    // Obtém o valor do atributo data-section
+    let sectionId = this.getAttribute("data-section");
+    let targetSection = document.getElementById(sectionId);
+    console.log("Section ID:", sectionId);
+    console.log("Target Section:", targetSection);
+    if (targetSection) {
+      // Obtém o offset ajustado ou usa 80 como padrão
+      let offset = targetSection.offsetTop - (sectionOffsets[sectionId] || 20);
 
-
-/* ============ SKILLS (MOVIMENTAÇAO)============ */
+      // Scroll suave até a seção
+      window.scrollTo({
+        top: offset,
+        behavior: "smooth",
+      });
+    } else {
+      console.error(`Seção com ID "${sectionId}" não encontrada.`);
+    }
+  });
+});
+/* ============ SKILLS (MOVIMENTAÇAO/ANIMACOES) ============ */
 
 // Função para verificar se o elemento está visível
 function isElementVisible(element) {
@@ -49,35 +59,8 @@ document.addEventListener("scroll", handleScroll);
 // Verifica a visibilidade ao carregar a página
 handleScroll();
 
-/* ============ HEADER MENUS SCROLL DOWN============ */
-const sectionOffsets = {
-  "skills": 50,
-  "trabalhos": 140,
-  "sobre-mim": 100,
-  "contato": 50,
-};
+/* ============ SKILLS -- ABA/PROJETOS  ============ */
 
-document.querySelectorAll(".header-menu li").forEach((item) => {
-  item.addEventListener("click", function () {
-    // Converte o texto do menu para minúsculas e substitui espaços por hífens
-    let sectionId = this.textContent.trim().toLowerCase().replace(/\s+/g, "-");
-    let targetSection = document.getElementById(sectionId);
-
-    if (targetSection) {
-      let offset = targetSection.offsetTop - (sectionOffsets[sectionId] || 80);
-      // Se não houver ajuste específico, usa 80 como padrão
-
-      window.scrollTo({
-        top: offset,
-        behavior: "smooth",
-      });
-    } else {
-      console.error(`Seção com ID "${sectionId}" não encontrada.`);
-    }
-  });
-});
-
-/* ============ ABA/PROJETOS SKILLS ============ */
 // Seleciona todas as abas
 const abas = document.querySelectorAll('.aba');
 
@@ -116,59 +99,8 @@ document.addEventListener('click', (event) => {
   }
 });
 
-/* ============ CARDS SKILLS ============ */
+// ====================SKILLS CARDS =====================
 
-
-
-/* ============ TRABALHOS REALIZADOS ============ */
-
-const setaClique = document.querySelector(".seta");
-const card2 = document.getElementById("card2");
-
-setaClique.addEventListener("click", () => {
-  card2.classList.toggle("active");
-});
-
-/* ============ PORTFOLIO FOTOGRAFIA ============ */
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   const img = document.querySelector("#portfolio-fotografia img");
-//   const h3 = document.querySelector("#portfolio-fotografia h3");
-
-//   img.addEventListener("mouseenter", () => {
-//     h3.classList.add("ativo");
-//   });
-
-//   img.addEventListener("mouseleave", () => {
-//     h3.classList.remove("ativo");
-//   });
-// });
-
-// document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-//   anchor.addEventListener("click", function (e) {
-//     e.preventDefault();
-
-//     const targetElement = document.querySelector(this.getAttribute("href"));
-
-//     window.scrollTo({
-//       top: targetElement.offsetTop,
-//       behavior: "smooth",
-//     });
-//   });
-// });
-// ====================HEADER HAMBURGUER===============
-
-// Seleciona o ícone do hamburguer e o menu
-const hamburguer = document.querySelector('.header-hamburguer');
-const menu = document.querySelector('.header-menu');
-
-// Adiciona um evento de clique ao hamburguer
-hamburguer.addEventListener('click', () => {
-  menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
-});
-// ====================skills cards===============
-// Seleciona todos os cards
-// Seleciona todos os cards
 const skillsCards = document.querySelectorAll('.skills-card');
 
 // Adiciona evento de clique para cada card
@@ -210,3 +142,58 @@ document.addEventListener('click', () => {
     }
   });
 });
+
+
+/* ============ TRABALHOS REALIZADOS ============ */
+
+const setaClique = document.querySelector(".seta");
+const card2 = document.getElementById("card2");
+
+setaClique.addEventListener("click", () => {
+  card2.classList.toggle("active");
+});
+
+// ====================HEADER HAMBURGUER===============
+
+// Seleciona o ícone do hamburguer e o menu
+const hamburguer = document.querySelector('.header-hamburguer');
+const menu = document.querySelector('.header-menu');
+
+// Adiciona um evento de clique ao hamburguer
+hamburguer.addEventListener('click', () => {
+  menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
+});
+
+
+
+/* ============ PORTFOLIO FOTOGRAFIA ============ */
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const img = document.querySelector("#portfolio-fotografia img");
+//   const h3 = document.querySelector("#portfolio-fotografia h3");
+
+//   img.addEventListener("mouseenter", () => {
+//     h3.classList.add("ativo");
+//   });
+
+//   img.addEventListener("mouseleave", () => {
+//     h3.classList.remove("ativo");
+//   });
+// });
+
+// document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+//   anchor.addEventListener("click", function (e) {
+//     e.preventDefault();
+
+//     const targetElement = document.querySelector(this.getAttribute("href"));
+
+//     window.scrollTo({
+//       top: targetElement.offsetTop,
+//       behavior: "smooth",
+//     });
+//   });
+// });
+
+/* ============ ENTRADA============ */
+
+
